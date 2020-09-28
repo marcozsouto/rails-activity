9b70a91970982107341354e6e3d76602ff654dee
marcozsouto <marcozsouto@gmail.com>
2020-09-28 13:02:32 -0300

    user-test 1, 2, 3 and 4

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,5 +18,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.update email:"lisa"
   end
 
-       
+  test 'username has to be unique' do 
+    user = User.new username:"jisoo", email:"jisoo@gmail.com", password:"654321", password_confirmation:"654321"
+    assert user.save
+    user2 = User.new username:"jisoo", email:"lisa@gmail.com", password:"123456", password_confirmation:"123456"
+    assert_not user2.save
+  end      
 end
