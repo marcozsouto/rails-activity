018cd318d6bc9d976e17fcfe9b450a10698f2300
marcozsouto <marcozsouto@gmail.com>
2020-09-28 11:20:21 -0300

    updated validates

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,5 +1,10 @@
 class Complaint < ApplicationRecord
     belongs_to :user
+    validates :title, presence: true, length: { in: 5..25 }
+    validates :text, presence: true, length: { in: 15..1280 }
+    validates :category, presence: true, length: { in: 4..25}
+    validates :company, presence: true
+
 
     def self.search(search, search_company)
         if search and search_company
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :complaint
   validates :username, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
