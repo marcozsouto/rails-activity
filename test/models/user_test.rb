7abbc5a2b0daffcc498794fea2652992c855f306
marcozsouto <marcozsouto@gmail.com>
2020-09-28 12:59:41 -0300

    user-test 1 and 2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,5 +7,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.save
   end
 
-       
+  test 'should not save user with invalid email' do
+    user = User.new username:"jennie", email:"jennie", password:"123456", password_confirmation:"123456"
+    assert_not user.save
+  end
+
+    
 end
