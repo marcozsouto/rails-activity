77a2ea16f3a2af5ac600adb0f0b3bd55967e1846
marcozsouto <marcozsouto@gmail.com>
2020-09-28 12:57:27 -0300

    user-test 1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test 'should not save user without username' do
+    user = User.new username:"", email:"rose@gmail.com", password:"123456", password_confirmation:"123456"
+    assert_not user.save
+  end
+
+       
 end
