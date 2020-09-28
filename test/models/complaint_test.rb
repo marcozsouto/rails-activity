735d7bb7770a1a4d4163a371ea99a5f29baf4e5d
marcozsouto <marcozsouto@gmail.com>
2020-09-28 13:21:41 -0300

    complaint-test 1 and 2

--- test/models/complaint_test.rb
+++ test/models/complaint_test.rb
@@ -9,6 +9,13 @@ class ComplaintTest < ActiveSupport::TestCase
     assert_not complaint.save
   end
 
-  
+  test 'should not edit complaint with text violating minimun lengh' do
+    user = User.new username:"jisoo", email:"jisoo@gmail.com", password:"654321", password_confirmation:"654321"
+    assert user.save
+    complaint = Complaint.new title:"title", text:"minimum lengh is 15chars", category:"minimum lengh is 4chars", company:"Empresa1", user_id: user.id
+    assert complaint.save
+    assert_not complaint.update text:"too small"
+  end
+
 
 end
