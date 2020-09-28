65667e66684d41432db13ab17131ee5b16d5962c
marcozsouto <marcozsouto@gmail.com>
2020-09-28 11:03:51 -0300

    scenario create user + edit user

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -17,4 +17,32 @@ Then("i see a created user message") do
     expect(page).to have_content("User was successfully created.")
 end
 
+#FOR SCENARIO edit user 
 
+Given("i am logged with the user whose username is {string}") do |username|
+    visit 'signup'
+    fill_in 'user[username]', :with => username
+    fill_in 'user[email]', :with => username + '@gmail.com'  
+    fill_in 'user[password]', :with => '123456'
+    fill_in 'user[password_confirmation]', :with => '123456'
+    click_button 'submit'
+    visit '/logout'
+    visit '/login'
+    fill_in 'username', :with => 'rose'
+    fill_in 'password', :with => '123456'
+    click_button 'login'
+end
+  
+And("i am on the user's edit page with the username is {string}") do |username|
+    visit'/users'
+    click_link"e-#{username}"
+
+end
+
+And("i click edit") do
+    click_button 'submit'
+end
+
+Then("i see a updated user message") do
+    expect(page).to have_content("User was successfully updated.")
+end
--- features/user.feature
+++ features/user.feature
@@ -9,4 +9,9 @@ Feature:  user
         And i click create
         Then i see a created user message
 
-    
+    Scenario: edit user 
+        Given i am logged with the user whose username is 'rose'
+        And i am on the user's edit page with the username is 'rose'
+        When i fill username 'jennie', email 'jennie@gmail.com', password '123456' and password confirmation '123456'
+        And i click edit
+        Then i see a updated user message
