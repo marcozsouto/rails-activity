f1a0f799707ae82ece1e82a8327d8d8b52fc2803
marcozsouto <marcozsouto@gmail.com>
2020-09-28 13:20:44 -0300

    complaint-test 1

--- test/models/complaint_test.rb
+++ test/models/complaint_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ComplaintTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test'should not save complaint with title violating minimum lengh' do
+    user = User.new username:"jisoo", email:"jisoo@gmail.com", password:"654321", password_confirmation:"654321"
+    assert user.save
+    complaint = Complaint.new title:"aaa", text:"minimum lengh is 15chars", category:"minimum lengh is 4chars", company:"Empresa1", user_id: user.id
+    assert_not complaint.save
+  end
+
+  
+
 end
